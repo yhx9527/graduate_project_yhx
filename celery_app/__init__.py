21d93e0e193ad07bc7adb778f833201bbfd58710
yhx9527 <540230904@qq.com>
2020-04-15 11:37:04 +0800

    修改celery配置

--- celery_app/__init__.py
+++ celery_app/__init__.py
@@ -5,6 +5,11 @@ from config import REDIS_URL_CELERY
 config = {
     'CELERY_BROKER_URL': REDIS_URL_CELERY,
     'CELERY_RESULT_BACKEND': REDIS_URL_CELERY,
+    'CELERYD_CONCURRENCY':10, # 并发worker数
+    'CELERYD_FORCE_EXECV': True, # 非常重要,有些情况下可以防止死锁
+    'CELERYD_MAX_TASKS_PER_CHILD': 100, # 每个worker最多执行万100个任务就会被销毁，可防止内存泄露
+    'CELERY_DISABLE_RATE_LIMITS': True, # 任务发出后，经过一段时间还未收到acknowledge , 就将任务重新交给其他worker执行
+
 }
 celery = Celery('tasks', broker=config['CELERY_BROKER_URL'])
 
